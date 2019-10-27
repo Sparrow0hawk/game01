94969b63213fa6ea69dc6f7c8b014f4f2fb61f11
sparrow0hawk <alexjcoleman@gmail.com>
2019-10-27 19:23:36 +0000

    drawing static shapes onto the canvas

--- js/game.js
+++ js/game.js
@@ -5,3 +5,12 @@ var context = canvas.getContext("2d");
 
 context.fillStyle = "red";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+// addition game boxes
+
+context.fillStyle = "grey";
+context.fillRect(5, 5, 10, 15);
+context.fillStyle = "blue";
+context.fillRect(25, 25, 20, 20);
+context.fillStyle = "green";
+context.fillRect(50, 50, 20, 40);
