86b42cc1aff5d9b4bcf396aa13ab39fbe3684c05
sparrow0hawk <alexjcoleman@gmail.com>
2019-10-27 18:54:38 +0000

    add a red canvas

---
+++ js/game.js
@@ -0,0 +1,7 @@
+// game js
+
+var canvas = document.getElementById("game-layer");
+var context = canvas.getContext("2d");
+
+context.fillStyle = "red";
+context.fillRect(0, 0, canvas.width, canvas.height);
